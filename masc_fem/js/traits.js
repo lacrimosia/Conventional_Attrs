61f164f4b96da9af10175aefbde54be8839c8a00
Shatilla Prayer <shatilla.prayer@unlv.edu>
2016-06-22 09:15:51 -0700

    Added trait type and sub categories

--- masc_fem/js/traits.js
+++ masc_fem/js/traits.js
@@ -3,10 +3,11 @@
 
 // Get our data
 var app = {}; // define globally
+var masculine = [];  // gather all masculine selected traits
+var feminine = [];   // gather all feminine traits
+
 $.getJSON("data/data.json", function(data) {
     app = data;
-
-
     init(); // defaults
 
     // reload app 
@@ -37,7 +38,7 @@ $.getJSON("data/data.json", function(data) {
 
     function showAllTraits() {
         for (var item = 0; item < app.traits.length; item++) {
-            $('.traits').append("<button class='traitsButton' id=" + item + "><i class='fa fa-check-square-o'></i>&nbsp;" + app.traits[item].name + "</button>");
+            $('.traits').append("<button class='traitsButton' id=" + item + ">" + app.traits[item].name + "</button>");
         }
         return item;
     }
@@ -58,26 +59,40 @@ $.getJSON("data/data.json", function(data) {
         // toggle selection
         app.traits[current].selected = !app.traits[current].selected;
         // toggle classes on click
-        traitToggle(app.traits[current].selected, Id);
+        traitToggle(app.traits[current].selected, Id, app.traits[current].type);
         return current;
     }
 
-    function traitToggle(selection, ob) {
+    function traitToggle(selection, ob, trait) {
         // selection - the current selected key
+        // True - element selected, False - element not selected
         // ob - the selected element       
         // if selected add class green, if button is unselcted add class red
         if(selection == true){
-            $('#'+ob).addClass('green');
-            console.log($('#'+ob));
+            $('#'+ob).addClass('green'); // add class green
+            checkType(trait);
             if($('#'+ob).hasClass('red')){
-                $('#'+ob).removeClass('red');
+                $('#'+ob).removeClass('red'); // remove class red if already added
             }
+
         }else{
-            $('#'+ob).addClass('red');
+            $('#'+ob).addClass('red');  // add class red
             if($('#'+ob).hasClass('green')){
-                $('#'+ob).removeClass('green');
+                $('#'+ob).removeClass('green'); // remove class green if already added
             }
         }
+    }
+
+    function checkType(trait){
+        // check type of trait selected and push into proper array
+        if(trait === 'masculine'){
+            masculine.push(trait);
+        }else if(trait === 'feminine'){
+            feminine.push(trait);
+        }else{
+            console.log("no trait type");
+        }
+       // console.log('masculine', masculine);
     }
 
 });
