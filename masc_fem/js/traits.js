19de2ba177f79c2dfc4a10f4d33b6076cdb46a80
Shatilla Prayer <shatilla.prayer@unlv.edu>
2016-06-27 12:10:35 -0700

    Replaced files after deletion

--- masc_fem/js/traits.js
+++ masc_fem/js/traits.js
@@ -15,273 +15,287 @@ var showHelp = false; // show help when clicked or key is pressed
 var openHelp = false; // whether help menu is open or closed
 var userSelect = []; // this pushes the selected traits
 
-$.getJSON("data/data.json", function(data) {
-    app = data;
-    app.shuffleArray = _.shuffle(app.traits); // shuffle our traits each time
-    init(); // defaults
+$(document).ready(function() {
+    $.getJSON("data/data.json", function(data) {
+        app = data;
+        app.shuffleArray = _.shuffle(app.traits); // shuffle our traits each time
+        init(); // defaults
 
-    // reload app 
-    $('.reload').click(function() {
-        location.reload();
-    });
-
-    // help menu will show
-    $('.help').click(function() {
-        showHelpMenu();
-        $('.help_Button').html("Close");
-    });
-
-    // close help menu
-    $('.help_Button').click(function() {
-        closeHelpMenu();
-    });
-
-    //show results
-    $('.finish').click(function() {
-        finish();
-    });
-
-    $('.traitsButton').click(function() {
-        getIdSelection($(this).attr('id'));
-    });
-
-    function init() {
-        loadButtons();
-        $('.final').hide();
-    }
-
-    function loadButtons() {
-        for (var item = 0; item < app.shuffleArray.length; item++) {
-            $('.traits').append("<div class='sele " + (item + 1) + "'><button class='traitsButton " + app.shuffleArray[item].type + "' id='" + item + "' tabindex='" + (item + 1) + "'><i class='fa fa-plus'></i></button><span for='" + item + "'>" + app.shuffleArray[item].name + "</span></div>");
-        }
-    }
-
-    function getIdSelection(Id) {
-        // get the id of the currently selected button
-        // when clicked find matching item in array and change selected to true
-        // match id with app.name
-        var current = 0;
-        var shuffleArray = app.shuffleArray;
-        _.forEach(shuffleArray, function(value, key) {
-            //key = index
-            // value = each object
-            if (key == Id) {
-                current = Id;
-                // console.log("current key", current);
-                // console.log("selected State current key", shuffleArray[current].selected);
-            }
+        // reload app 
+        $('.reload').click(function() {
+            location.reload();
         });
-        // toggle selection
-        shuffleArray[current].selected = !shuffleArray[current].selected;
-        //  console.log("when clicked", shuffleArray[current].name + ': ' + shuffleArray[current].selected);
-        // toggle classes on click
-        traitToggle(shuffleArray[current].selected, Id, shuffleArray[current].type, shuffleArray[current].name);
 
-        // checks if any traits are selected or not
-        if (masculine == 0 && feminine == 0) {
-            selected = false;
-        } else {
-            selected = true;
+        // help menu will show
+        $('.help').click(function() {
+            showHelpMenu();
+            $('.help_Button').html("Close");
+        });
+
+        // close help menu
+        $('.help_Button').click(function() {
+            closeHelpMenu();
+        });
+
+        //show results
+        $('.finish').click(function() {
+            finish();
+        });
+
+        $('.traitsButton').click(function() {
+            getIdSelection($(this).attr('id'));
+        });
+
+        function init() {
+            loadButtons();
+            $('.final').hide();
+            $('#0').focus();
         }
 
-        return current;
-    }
+        function loadButtons() {
+            for (var item = 0; item < app.shuffleArray.length; item++) {
+                $('.traits').append("<div class='sele " + (item + 1) + "'><button class='traitsButton " + app.shuffleArray[item].type + "' id='" + item + "' tabindex='" + (item + 1) + "'><i class='fa fa-times-circle'></i></button><span for='" + item + "'>" + app.shuffleArray[item].name + "</span></div>");
+            }
+        }
 
-    function traitToggle(selection, ob, trait, name) {
-        // selection - the current selected key
-        // True - element selected, False - element not selected
-        // ob - the selected element       
-        // if selected add class green, if button is unselcted add class red
-        if (selection == true) {
-            $('#' + ob).addClass('green'); // add class green
-            $('#' + ob).html('<i class="fa fa-minus"></i>');
-            selectedTraits(trait);
+        function getIdSelection(Id) {
+            // get the id of the currently selected button
+            // when clicked find matching item in array and change selected to true
+            // match id with app.name
+            var current = 0;
+            var shuffleArray = app.shuffleArray;
+            _.forEach(shuffleArray, function(value, key) {
+                //key = index
+                // value = each object
+                if (key == Id) {
+                    current = Id;
+                    // console.log("current key", current);
+                    // console.log("selected State current key", shuffleArray[current].selected);
+                }
+            });
+            // toggle selection
+            shuffleArray[current].selected = !shuffleArray[current].selected;
+            //  console.log("when clicked", shuffleArray[current].name + ': ' + shuffleArray[current].selected);
+            // toggle classes on click
+            traitToggle(shuffleArray[current].selected, Id, shuffleArray[current].type, shuffleArray[current].name);
 
-            // push user selected traits to show later
+            // checks if any traits are selected or not
+            if (masculine == 0 && feminine == 0) {
+                selected = false;
+            } else {
+                selected = true;
+            }
+
+            return current;
+        }
+
+        function traitToggle(selection, ob, trait, name) {
+            // selection - the current selected key
+            // True - element selected, False - element not selected
+            // ob - the selected element       
+            // if selected add class green, if button is unselcted add class red
+            if (selection == true) {
+                $('#' + ob).addClass('green'); // add class green
+                $('#' + ob).html('<i class="fa fa-check-circle"></i>');
+                selectedTraits(trait);
+
+                // push user selected traits to show later
+                userSelect.push({
+                    choice: name,
+                    type: trait
+                });
+                //  console.log('userSelect', userSelect);
+
+                if ($('#' + ob).hasClass('red')) {
+                    $('#' + ob).removeClass('red'); // remove class red if already added
+                }
+
+            } else {
+                $('#' + ob).addClass('red'); // add class red
+                $('#' + ob).html('<i class="fa fa-times-circle"></i>');
+                unselectedTraits(trait);
+                // find unselected trait and remove from list
+                // var findRemoveTrait = _.findIndex(userSelect, {'choice': name, 'type': trait});
+
+                var removeElement = _.reject(userSelect, { 'choice': name, 'type': trait });
+                //  console.log('removeElement', removeElement);
+                userSelect = removeElement;
+
+                console.log('removed userSelect', userSelect);
+
+                if ($('#' + ob).hasClass('green')) {
+                    $('#' + ob).removeClass('green'); // remove class green if already added
+                }
+                return userSelect;
+            }
+        }
+
+        function selectedTraits(trait) {
+            // check type of trait selected and push into proper array
+            if (trait === 'masculine') {
+                masculine += 1;
+                console.log('masculine', masculine);
+                return masculine;
+            } else if (trait === 'feminine') {
+                feminine += 1;
+            } else {
+                console.log("no trait type");
+            }
+        }
+
+        function unselectedTraits(trait) {
+            // check type of trait selected and push into proper array
+            if (trait === 'masculine' && masculine > 0) {
+                masculine -= 1;
+                console.log('masculine', masculine);
+                return masculine;
+            } else if (trait === 'feminine' && feminine > 0) {
+                feminine -= 1;
+            } else {
+                console.log("no trait type");
+            }
+        }
+
+        function finish() {
+            if (selected == true) {
+                $('.final').show();
+                $('.traits').hide();
+                $('.finish').hide();
+                $('.selTraitEmerge').text('').fadeIn();
+                displayListFinal();
+
+                Chart.defaults.global.defaultFontColor = '#C1C1C1'; // font color 
+                Chart.defaults.global.title.fontSize = 14; // chart font size
+
+                // chart
+                var myChart = new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ["Masculine", "Feminine"],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [masculine, feminine],
+                            backgroundColor: [
+                                'rgba(212, 0, 29, 1)',
+                                'rgba(239, 151, 25, 1)'
+                            ],
+                            borderColor: [
+                                'rgba(212, 0, 29, 1)',
+                                'rgba(239, 151, 25, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        },
+                        responsive: false,
+                        showTooltip: true,
+                        gridLines: {
+                            display: true
+                        },
+                        title: {
+                            display: true,
+                            text: 'Conventional Attributes Results'
+                        },
+                        hover: {
+                            mode: 'label'
+                        }
+                    }
+                });
+
+            } else {
+                //  alert("Please select a trait");
+                // $('.finish').prop('disabled');
+                $('.selTraitEmerge').text('Please select a trait').fadeIn();
+            }
+        }
+
+        $('#returnButton').click(function() {
+            $('.help_Menu').hide();
+        });
+
+        /* selected array, selected item */
+        function showTally(trait, name) {
             userSelect.push({
                 choice: name,
                 type: trait
             });
             console.log('userSelect', userSelect);
+        }
 
-            if ($('#' + ob).hasClass('red')) {
-                $('#' + ob).removeClass('red'); // remove class red if already added
+        function displayListFinal() {
+            for (var t = 0; t < userSelect.length; t++) {
+                $('.theTraits').append('<div class="uselect u' + userSelect[t].type + '">' + userSelect[t].choice + '</div>');
             }
-
-        } else {
-            $('#' + ob).addClass('red'); // add class red
-            $('#' + ob).html('<i class="fa fa-plus"></i>');
-            unselectedTraits(trait);
-            // find unselected trait and remove from list
-            // var findRemoveTrait = _.findIndex(userSelect, {'choice': name, 'type': trait});
-
-            var removeElement = _.reject(userSelect, { 'choice': name, 'type': trait });
-            //  console.log('removeElement', removeElement);
-            userSelect = removeElement;
-
-            console.log('removed userSelect', userSelect);
-
-            if ($('#' + ob).hasClass('green')) {
-                $('#' + ob).removeClass('green'); // remove class green if already added
-            }
-            return userSelect;
         }
-    }
 
-    function selectedTraits(trait) {
-        // check type of trait selected and push into proper array
-        if (trait === 'masculine') {
-            masculine += 1;
-            console.log('masculine', masculine);
-            return masculine;
-        } else if (trait === 'feminine') {
-            feminine += 1;
-        } else {
-            console.log("no trait type");
-        }
-    }
+        $(document).bind('keyup', function(e) {
+            key = e.keyCode;
 
-    function unselectedTraits(trait) {
-        // check type of trait selected and push into proper array
-        if (trait === 'masculine' && masculine > 0) {
-            masculine -= 1;
-            console.log('masculine', masculine);
-            return masculine;
-        } else if (trait === 'feminine' && feminine > 0) {
-            feminine -= 1;
-        } else {
-            console.log("no trait type");
-        }
-    }
-
-    function finish() {
-        if (selected == true) {
-            $('.final').show();
-            $('.traits').hide();
-            displayListFinal();
-
-            // chart
-            var myChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ["Masculine", "Feminine"],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [masculine, feminine],
-                        backgroundColor: [
-                            'rgba(27, 31, 78, 1)',
-                            'rgba(237, 89, 1, 1)'
-                        ],
-                        borderColor: [
-                            'rgba(255,255,255,1)',
-                            'rgba(255, 255, 255, 1)'
-                        ],
-                        borderWidth: 2
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    },
-                    responsive: false,
-                    showTooltip: true,
-                    gridLines: {
-                        display: true
-                    },
-                    title: {
-                        display: true,
-                        text: 'Conventional Attributes Results'
-                    },
-                    hover: {
-                        mode: 'label'
-                    }
-                }
-            });
-
-        } else {
-            alert("Please select a trait");
-
-        }
-    }
-
-    /* selected array, selected item */
-    function showTally(trait, name) {
-        userSelect.push({
-            choice: name,
-            type: trait
-        });
-        console.log('userSelect', userSelect);
-    }
-
-    function displayListFinal() {
-        for (var t = 0; t < userSelect.length; t++) {
-            $('.theTraits').append('<div class="uselect ' + userSelect[t].type + '">' + userSelect[t].choice + '</div>');
-        }
-    }
-
-    $(document).bind('keyup', function(e) {
-        key = e.keyCode;
-
-        if (key == 82) {
-            location.reload(); //reload app - r key
-        } else if (key == 83) {
-            intro(); //Start App - s key
-        } else if (key == 9 && disableKey == false) {
-            e.preventDefault();
-            // tab key
-            var tabIndex = $('.traitsButton:focus').attr('tabindex');
-            var totalSize = $('.traitsButton').size();
-            console.log(tabIndex);
-
-            if (tabIndex == undefined) {
-                //   console.log('total', totalSize);
-                //  tabIndex = $('.traitsButton:focus').attr('tabindex', 1);
-                $('#0').focus();
-                //  console.log('end', tabIndex);
-            } else {
-                tabIndex = $('.traitsButton:focus').attr('tabindex');
-                $('.sele ' + parseInt(tabIndex)).css('background-color', 'yellow !important');
-                console.log('pink area', tabIndex);
-                totalSize = $('.traitsButton').size();
-            }
-
-        } else if (key == 72 && disableKey == false) {
-            // H key - Help Menu
-            // toggles sound and changes icon based on whether sound is on or off
-            openHelp = !openHelp;
-            if (openHelp == true) {
-                showHelpMenu();
-                $('.help_Button').html("Close <i class='fa fa-times'></i>");
-            } else if (key == 32 && disableKey == false) {
+            if (key == 82) {
+                location.reload(); //reload app - r key
+            } else if (key == 83) {
+                intro(); //Start App - s key
+            } else if (key == 9 && disableKey == false) {
                 e.preventDefault();
-                // space to select button
-                getIdSelection($('sele.traitsButton').attr('tabindex'));
-                // if($('sele.traitsButton').attr('tabindex') == app.shuffleArray.length + parseInt(1)){
-                // }
-            } else {
-                closeHelpMenu();
+                // tab key
+                var tabIndex = $('.traitsButton:focus').attr('tabindex');
+                var totalSize = $('.traitsButton').size();
+                console.log(tabIndex);
+
+                if (tabIndex == undefined) {
+                    //   console.log('total', totalSize);
+                    //  tabIndex = $('.traitsButton:focus').attr('tabindex', 1);
+                    $('#0').focus();
+                    //  console.log('end', tabIndex);
+                } else {
+                    tabIndex = $('.traitsButton:focus').attr('tabindex');
+                    $('.sele ' + parseInt(tabIndex)).css('background-color', 'yellow !important');
+                    console.log('pink area', tabIndex);
+                    totalSize = $('.traitsButton').size();
+                }
+
+            } else if (key == 72 && disableKey == false) {
+                // H key - Help Menu
+                // toggles sound and changes icon based on whether sound is on or off
+                openHelp = !openHelp;
+                if (openHelp == true) {
+                    showHelpMenu();
+                    $('.help_Button').html("Close <i class='fa fa-times'></i>");
+                } else if (key == 32 && disableKey == false) {
+                    e.preventDefault();
+                    // space to select button
+                    getIdSelection($('sele.traitsButton').attr('tabindex'));
+                    // if($('sele.traitsButton').attr('tabindex') == app.shuffleArray.length + parseInt(1)){
+                    // }
+                } else {
+                    closeHelpMenu();
+                }
             }
+        });
+
+
+
+        function showHelpMenu() {
+            $('.help_Menu').show();
         }
+
+        function closeHelpMenu() {
+            $('.help_Menu').hide();
+        }
+
+        function intro() {
+            $('.help_Menu').hide();;
+            enabled = true;
+            disableKey = false;
+        }
+
     });
-
-
-
-    function showHelpMenu() {
-        $('.help_Menu').show();
-    }
-
-    function closeHelpMenu() {
-        $('.help_Menu').hide();
-    }
-
-    function intro() {
-        $('.help_Menu').hide();;
-        enabled = true;
-        disableKey = false;
-    }
 
 });
